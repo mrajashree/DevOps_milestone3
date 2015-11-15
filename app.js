deacf585dfab641a6b3a7713df81cbf68e17e90b
Rajashree Mandaogane <rajashree.28m@gmail.com>
2015-11-15 17:12:13 -0500

    app and proxy

---
+++ app.js
@@ -0,0 +1,135 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  routes = require('./routes'),
+  user = require('./routes/user'),
+  http = require('http'),
+  path = require('path'),
+  io = require('socket.io-client'),
+  os = require('os');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || process.argv[2]);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+/*app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home'
+  })
+});*/
+if(app.get('port') == '3000')
+  var message = 'Production! (Stable)'
+else
+  var message = 'Canary'
+
+app.get('/',function(req, res) { 
+  res.writeHead(200, {'content-type':'text/html'});
+    res.write(message);
+});
+
+app.get('/home',function(req, res) {
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+app.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+if(app.get('port') == '3000')
+  name = 'stable'
+else
+  name = 'canary'
+
+var socket = io.connect('http://127.0.0.1:4000');
+socket.on('connect', function () { 
+  console.log("socket connected"); 
+  setInterval(function() 
+  {
+    socket.emit('heartbeat',
+    {
+      Name: name, cpu: cpuAverage(), memoryLoad: memoryLoad(),
+    });
+  },2000);
+});
+
+function memoryLoad()
+{
+  console.log( os.totalmem(), os.freemem() );
+  var mem_used = (os.totalmem() - os.freemem())/os.totalmem();
+  console.log(mem_used * 100);
+  mem_used = mem_used*100;
+  return mem_used;
+}
+
+function cpuTicksAcrossCores() 
+{
+  //Initialise sum of idle and time of cores and fetch CPU info
+  var totalIdle = 0, totalTick = 0;
+  var cpus = os.cpus();
+ 
+  //Loop through CPU cores
+  for(var i = 0, len = cpus.length; i < len; i++) 
+  {
+    //Select CPU core
+    var cpu = cpus[i];
+    //Total up the time in the cores tick
+    for(type in cpu.times) 
+    {
+      totalTick += cpu.times[type];
+    }     
+    //Total up the idle time of the core
+    totalIdle += cpu.times.idle;
+  }
+ 
+  //Return the average Idle and Tick times
+  return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+}
+
+var startMeasure = cpuTicksAcrossCores();
+
+function cpuAverage()
+{
+  var endMeasure = cpuTicksAcrossCores(); 
+ 
+  //Calculate the difference in idle and total time between the measures
+  var idleDifference = endMeasure.idle - startMeasure.idle;
+  var totalDifference = endMeasure.total - startMeasure.total;
+ 
+  avg_cpu_usage = ((totalDifference - idleDifference) / totalDifference) * 100;
+  //Calculate the average percentage CPU usage
+  console.log(avg_cpu_usage)
+  return avg_cpu_usage;
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
---
+++ proxy_server.js
@@ -0,0 +1,58 @@
+var http      = require('http');
+var httpProxy = require('http-proxy');
+var redis = require('redis')
+var express = require('express')
+var app = express()
+var spawn = require('child_process').spawn;
+var client = redis.createClient(6379, '127.0.0.1', {})
+var socket_io = require('socket.io')
+var os = require('os')
+
+ports = ['3000','3001'];
+client.del('hosts')
+
+//Canary = 3001
+//Stable = 3000
+
+for(i in ports)
+{
+	console.log('http://127.0.0.1:'+ports[i])
+	client.lpush(['hosts','http://127.0.0.1:'+ports[i]],function(err, value) {
+		console.log("VALUE : ",value)
+	})
+}
+
+var options = {};
+var proxy   = httpProxy.createProxyServer(options);
+
+var server  = http.createServer(function(req, res)
+{
+	client.rpoplpush('hosts','hosts',function(err,value) {
+		proxy.web( req, res, {target: value } );
+		console.log("VALUE rpoplpush: ",value)
+	})
+});
+server.listen(8080);
+
+var monitoring_server = app.listen(4000, function () {
+
+          var host = monitoring_server.address().address
+          var port = monitoring_server.address().port
+
+        console.log('Example app listening at http://%s:%s', host, port)
+})
+var io = socket_io.listen(monitoring_server);
+
+io.sockets.on('connection', function (socket) {
+		console.log('Client connected')
+        socket.on('heartbeat',function(data){
+              console.log(data);
+              if(data.Name=='canary' && data.cpu > 20)
+              {
+              		console.log("NOW STOP Canary")
+              		client.del('hosts')
+              		client.lpush(['hosts','http://127.0.0.1:3000'],function(err, value) {})
+              }
+		});
+	})
+
